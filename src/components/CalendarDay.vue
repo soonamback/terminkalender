1b481bc27c5bd9fae09c1bc7de7cd5b87d585ea6
soonamback <soonam.yi123@gmail.com>
2024-01-15 11:54:08 +0900

    add vue structure

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,24 @@
+<template>
+      <div class="card">
+              <div class="card-header text-center" role="button">
+                <strong>Montag</strong>
+              </div>
+              <div class="card-body">
+                <CalendarEvent />
+              </div>
+            </div>
+</template>
+
+<script>
+import CalendarEvent from "./CalendarEvent"
+export default {
+    name: "CalendarDay",
+    components: {
+        CalendarEvent,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="calender-week">
+          <div class="card-group">
+            <CalendarDay v-for="i in 7" :key="i" />
+        </div>
+        </div>
+</template>
+
+<script>
+import CalendarDay from "./CalendarDay";
+export default {
+    name: "CalendarWeek",
+    components: {
+        CalendarDay,
+    },
+}
+</script>
+
+<style>
+
+</style>
