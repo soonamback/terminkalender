96a14ebb2b1a8e465499e1d4e59e6891d3271485
soonamback <soonam.yi123@gmail.com>
2024-01-15 15:15:29 +0900

    add store

--- src/components/CalendarWeek.vue
+++ src/components/CalendarWeek.vue
@@ -1,18 +1,24 @@
 <template>
  <div id="calender-week">
           <div class="card-group">
-            <CalendarDay v-for="i in 7" :key="i" />
+            <CalendarDay v-for="day in calendarWeekData" :key="day.id" :day="day" />
         </div>
         </div>
 </template>
 
 <script>
 import CalendarDay from "./CalendarDay";
+import Store from "../store";
 export default {
     name: "CalendarWeek",
     components: {
         CalendarDay,
     },
+    data() { 
+        return {
+            calendarWeekData: Store.state.calendarWeekData
+        }
+    }
 }
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { calendarWeekData } from "./seed"
+import { reactive, readonly } from 'vue' 
+
+const state = reactive({
+    calendarWeekData
+})
+
+const getters = {};
+
+
+const mutations = {};
+
+export default {
+    state: readonly(state),
+    getters,
+    mutations
+}
